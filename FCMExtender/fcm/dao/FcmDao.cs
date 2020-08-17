8b867a700bf46d6615879fe196265073d934a1a2
Dario Pesole <dpesole@minsait.com>
2020-08-17 13:12:20 +0200

    Avanzamento 23/8/2017

--- FCMExtender/fcm/dao/FcmDao.cs
+++ FCMExtender/fcm/dao/FcmDao.cs
@@ -1,4 +1,5 @@
 
+using fcm.model;
 using System;
 using System.Collections.Generic;
 using System.Data.Odbc;
@@ -10,7 +11,7 @@ namespace fcm.dao
 
         private OdbcConnection conn;
 
-        public FcmDao(String filename) {
+        public FcmDao(string filename) {
             conn.ConnectionString = @"Driver={Microsoft Access Driver (*.mdb)};Dbq=" + filename + ";";
             conn.Open();
         }
@@ -20,228 +21,218 @@ namespace fcm.dao
             conn.Dispose();
         }
 
-        public string[] getListaCompetizioni() {
+        public string[] getListaCompetizioni()
+        {
             //logger.info("Estrazione dell'elenco delle competizioni");
-            OdbcCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT id, nome FROM competizione";
-            OdbcDataReader rea = cmd.ExecuteReader();
-
-            //estraggo l'elenco delle competizioni
-            List<String> comps = new List<String>();
-            while (rea.Read()) {
-                comps.Add(rea.GetString(1) + " - " + rea.GetString(2));
+            using (OdbcCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT id, nome FROM competizione";
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    List<string> comps = new List<string>();
+                    while (rea.Read())
+                    {
+                        comps.Add(rea.GetString(1) + " - " + rea.GetString(2));
+                    }
+                    return comps.ToArray();
+                }
             }
-            rea.Close();
-            return comps.ToArray();
         }
 
         public string[] getListaGironi(string competizione)
         {
             //logger.info("Estrazione dell'elenco dei gironi");
-            OdbcCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT id, nome FROM girone WHERE idcompetizione = " + competizione;
-            OdbcDataReader rea = cmd.ExecuteReader();
-
-            //estraggo l'elenco delle competizioni
-            List<String> girs = new List<String>();
-            while (rea.Read())
+            using (OdbcCommand cmd = conn.CreateCommand())
             {
-                string girName = rea.GetString(2);
-                girName = girName != null ? girName : "Senza Nome";
-                girs.Add(rea.GetString(1) + " - " + girName);
+                cmd.CommandText = "SELECT id, nome FROM girone WHERE idcompetizione = " + competizione;
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    List<string> girs = new List<string>();
+                    while (rea.Read())
+                    {
+                        string girName = rea.GetString(2);
+                        girName = girName != null ? girName : "Senza Nome";
+                        girs.Add(rea.GetString(1) + " - " + girName);
+                    }
+                    return girs.ToArray();
+                }
             }
-            rea.Close();
-            return girs.ToArray();
         }
 
-        public Dictionary<int, string> getSquadreGirone(String idGirone)
+        public Dictionary<int, string> getSquadreGirone(string idGirone)
         {
             //logger.info("Estrazione squadre");
-
-            OdbcCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT i.idsquadra, f.nome FROM iscritta i, fantasquadra f WHERE i.idsquadra=f.id AND i.idgirone = " + idGirone;
-            OdbcDataReader rea = cmd.ExecuteReader();
-
-            //estraggo l'elenco dei gironi
-            Dictionary<int, string> nomiteam = new Dictionary<int, string>();
-            while (rea.Read())
+            using (OdbcCommand cmd = conn.CreateCommand())
             {
-                nomiteam.Add(rea.GetInt32(1), rea.GetString(2));
+                cmd.CommandText = "SELECT i.idsquadra, f.nome FROM iscritta i, fantasquadra f WHERE i.idsquadra=f.id AND i.idgirone = " + idGirone;
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    Dictionary<int, string> nomiteam = new Dictionary<int, string>();
+                    while (rea.Read())
+                    {
+                        //estraggo l'elenco dei gironi
+                        nomiteam.Add(rea.GetInt32(1), rea.GetString(2));
+                    }
+                    return nomiteam;
+
+                }
             }
-            rea.Close();
-            return nomiteam;
         }
 
-    public Regole getRegoleCompetizione (String idCompetizione) throws PluginException{
-		logger.info("Estrazione delle regole della competizione");
-		try (
-				Statement stmt = conn.createStatement();
-				){
-			return new Regole(stmt, idCompetizione);
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
+        public Regole getRegoleCompetizione (string idCompetizione) {
+		    //logger.info("Estrazione delle regole della competizione");
+		    return new Regole(conn, idCompetizione);
+	    }
+
+        public List<int> getGiornate(string idGirone)
+        {
+            //logger.info("Estrazione lista giornate");
+            using (OdbcCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT idGiornata FROM incontro WHERE idGirone = " + idGirone + " GROUP BY idGiornata ";
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    List<int> giornate = new List<int>();
+                    while (rea.Read())
+                    {
+                        giornate.Add(rea.GetInt32(1));
+                    }
+                    return giornate;
+                }
+            }
+        }
+
+        public int[] getSquadreIscritte(string idGirone)
+        {
+            //logger.info("Estrazione squadre iscritte");
+            using (OdbcCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT idSquadra FROM iscritta where idgirone=" + idGirone;
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    List<int> squadre = new List<int>();
+                    while (rea.Read())
+                    {
+                        squadre.Add(rea.GetInt32(1));
+                    }
+                    return squadre.ToArray();
+                }
+            }
+        }
 	
-	public ArrayList<Integer> getGiornate (String idGirone) throws PluginException{
-		logger.info("Estrazione lista giornate");
-		try (
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT idGiornata FROM incontro WHERE idGirone = "+idGirone+ " GROUP BY idGiornata ");
-				){
+	    public List<Incontro> getIncontri (string idGirone, int idGiornata) throws PluginException{
+		    logger.info("Estrazione incontri");
+		    try (
+				    Statement stmt = conn.createStatement();
+				    ResultSet rs = stmt.executeQuery("SELECT id, idcasa, idfuori, idtipo FROM incontro WHERE idgirone = "+idGirone+" AND idGiornata = "+idGiornata);
+				    ){
 
-			ArrayList<Integer> giornate = new ArrayList<Integer>();
-			while (rs.next()){
-				giornate.add(rs.getInt(1));
-			}
-			return giornate;
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
+			    List<Incontro> listaIncontri = new ArrayList<>();
+			    while (rs.next()){
+				    Incontro inc = new Incontro();
+				    if (rs.getInt(4)==1){
+					    inc.fattoreCampo = true;
+				    }
+				    inc.idIncontro=rs.GetString(1);
+				    inc.casa = rs.GetString(2);
+				    inc.trasferta = rs.GetString(3);
+				    listaIncontri.add(inc);
+			    }
+			    return listaIncontri;
+		    } catch (SQLException e) {
+			    logger.severe(e.getMessage());
+			    throw new PluginException("");
+		    }
+	    }
 	
-	public Integer[] getSquadreIscritte (String idGirone) throws PluginException{
-		logger.info("Estrazione squadre iscritte");
-		try (
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT idSquadra FROM iscritta where idgirone="+idGirone);
-				){
+	    public Map<Integer, Tabellino> getTabellini (ArrayList<string> idIncontri) throws PluginException, InvalidGiornataException{
+		    logger.info("Estrazione tabellini");
+		    string filtro = idIncontri.tostring().replace("[", "(").replace("]", ")");
+		    try (
+				    Statement stmt = conn.createStatement();
+				    ResultSet rs = stmt.executeQuery("SELECT idsquadra, tot, ruolo, modportiere, modattacco, moddifesa, voto, modm1pers, modm2pers, modm3pers FROM tabellino WHERE idincontro IN "+filtro+" ORDER BY idsquadra");
+				    ){
 
-			ArrayList<Integer> squadre = new ArrayList<Integer>();
-			while (rs.next()){
-				squadre.add(rs.getInt(1));
-			}
-			Integer[] squadreArray = new Integer[squadre.size()];
-			squadre.toArray(squadreArray);
-			return squadreArray;
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
-	
-	public List<Incontro> getIncontri (String idGirone, int idGiornata) throws PluginException{
-		logger.info("Estrazione incontri");
-		try (
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT id, idcasa, idfuori, idtipo FROM incontro WHERE idgirone = "+idGirone+" AND idGiornata = "+idGiornata);
-				){
+			    Map<Integer, Tabellino> mapTabellini = new HashMap<>();
+			    boolean saltaGiornata = true;
+			    while (rs.next()){
+				    Tabellino tab = new Tabellino();
+				    string votistring = rs.GetString(2);
+				    if (votistring!=null){
+					    tab.voti = votistring.split("%");
+					    saltaGiornata = false;
+				    }
+				    string ruolistring = rs.GetString(3);
+				    if (ruolistring!=null){
+					    tab.ruoli = ruolistring.split("%");
+					    saltaGiornata = false;
+				    }
+				    tab.modPortiere = rs.getDouble(4);
+				    tab.modAttacco = rs.getDouble(5);
+				    tab.modDifesa = rs.getDouble(6);
 
-			List<Incontro> listaIncontri = new ArrayList<>();
-			while (rs.next()){
-				Incontro inc = new Incontro();
-				if (rs.getInt(4)==1){
-					inc.fattoreCampo = true;
-				}
-				inc.idIncontro=rs.getString(1);
-				inc.casa = rs.getString(2);
-				inc.trasferta = rs.getString(3);
-				listaIncontri.add(inc);
-			}
-			return listaIncontri;
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
-	
-	public Map<Integer, Tabellino> getTabellini (ArrayList<String> idIncontri) throws PluginException, InvalidGiornataException{
-		logger.info("Estrazione tabellini");
-		String filtro = idIncontri.toString().replace("[", "(").replace("]", ")");
-		try (
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT idsquadra, tot, ruolo, modportiere, modattacco, moddifesa, voto, modm1pers, modm2pers, modm3pers FROM tabellino WHERE idincontro IN "+filtro+" ORDER BY idsquadra");
-				){
-
-			Map<Integer, Tabellino> mapTabellini = new HashMap<>();
-			boolean saltaGiornata = true;
-			while (rs.next()){
-				Tabellino tab = new Tabellino();
-				String votiString = rs.getString(2);
-				if (votiString!=null){
-					tab.voti = votiString.split("%");
-					saltaGiornata = false;
-				}
-				String ruoliString = rs.getString(3);
-				if (ruoliString!=null){
-					tab.ruoli = ruoliString.split("%");
-					saltaGiornata = false;
-				}
-				tab.modPortiere = rs.getDouble(4);
-				tab.modAttacco = rs.getDouble(5);
-				tab.modDifesa = rs.getDouble(6);
-
-				String votipuriString = rs.getString(7);
-				if (votipuriString!=null){
-					tab.votipuri = votipuriString.split("%");
-					saltaGiornata = false;
-				}
+				    string votipuristring = rs.GetString(7);
+				    if (votipuristring!=null){
+					    tab.votipuri = votipuristring.split("%");
+					    saltaGiornata = false;
+				    }
 				
-				tab.modPers1 = rs.getDouble(8);
-				tab.modPers2 = rs.getDouble(9);
-				tab.modPers3 = rs.getDouble(10);
-				mapTabellini.put(rs.getInt(1), tab);
-			}
-			if (saltaGiornata){
-				throw new InvalidGiornataException();
-			}
-			return mapTabellini;
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
+				    tab.modPers1 = rs.getDouble(8);
+				    tab.modPers2 = rs.getDouble(9);
+				    tab.modPers3 = rs.getDouble(10);
+				    mapTabellini.put(rs.getInt(1), tab);
+			    }
+			    if (saltaGiornata){
+				    throw new InvalidGiornataException();
+			    }
+			    return mapTabellini;
+		    } catch (SQLException e) {
+			    logger.severe(e.getMessage());
+			    throw new PluginException("");
+		    }
+	    }
 	
-	public List<Fascia> getFasceConversioneGol (String idCompetizione) throws PluginException{
-		logger.info("Estrazione fasce gol");
-		return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellagol g, fascia f WHERE g.idCompetizione = "+idCompetizione+" AND g.idFascia=f.id");
-	}
+	    public List<Fascia> getFasceConversioneGol (string idCompetizione) throws PluginException{
+		    logger.info("Estrazione fasce gol");
+		    return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellagol g, fascia f WHERE g.idCompetizione = "+idCompetizione+" AND g.idFascia=f.id");
+	    }
 	
-	public List<Fascia> getFasceModificatoreDifesa (String idCompetizione) throws PluginException{
-		logger.info("Estrazione fasce modificatore difesa");
-		return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabelladifesa d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
-	}
+	    public List<Fascia> getFasceModificatoreDifesa (string idCompetizione) throws PluginException{
+		    logger.info("Estrazione fasce modificatore difesa");
+		    return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabelladifesa d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
+	    }
 	
-	public List<Fascia> getContributoNumeroDifensoriModificatoreDifesa (String idCompetizione) throws PluginException{
-		logger.info("Estrazione contributo numero difensori per modificatore difesa");
-		return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellanumdifensori d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
-	}
+	    public List<Fascia> getContributoNumeroDifensoriModificatoreDifesa (string idCompetizione) throws PluginException{
+		    logger.info("Estrazione contributo numero difensori per modificatore difesa");
+		    return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellanumdifensori d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
+	    }
 	
-	public List<Fascia> getFasceModificatoreCentrocampo (String idCompetizione) throws PluginException{
-		logger.info("Estrazione fasce modifcatore centrocampo");
-		return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellacentrocampodiffe d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
-	}
+	    public List<Fascia> getFasceModificatoreCentrocampo (string idCompetizione) throws PluginException{
+		    logger.info("Estrazione fasce modifcatore centrocampo");
+		    return getDatiDaFasceByQuery("SELECT f.valore, f.min, f.max FROM tabellacentrocampodiffe d, fascia f WHERE d.idcompetizione = "+idCompetizione+" AND d.idfascia=f.id");
+	    }
 	
-	private List<Fascia> getDatiDaFasceByQuery (String query) throws PluginException{
-		try (
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-				){
+	    private List<Fascia> getDatiDaFasceByQuery (string query) throws PluginException{
+		    try (
+				    Statement stmt = conn.createStatement();
+				    ResultSet rs = stmt.executeQuery(query);
+				    ){
 
-			List<Fascia> listaFasce = new ArrayList<>();
-			while (rs.next()){
-				Fascia fascia = new Fascia();
-				fascia.valore = rs.getDouble(1);
-				fascia.min = rs.getDouble(2);
-				fascia.max = rs.getDouble(3);
-				listaFasce.add(fascia);
-			}
-			return listaFasce;
-		} catch (SQLException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException("");
-		}
-	}
-
-	@Override
-	public void close() throws Exception {
-		if (conn!=null){
-			conn.close();
-		}
-	}
+			    List<Fascia> listaFasce = new ArrayList<>();
+			    while (rs.next()){
+				    Fascia fascia = new Fascia();
+				    fascia.valore = rs.getDouble(1);
+				    fascia.min = rs.getDouble(2);
+				    fascia.max = rs.getDouble(3);
+				    listaFasce.add(fascia);
+			    }
+			    return listaFasce;
+		    } catch (SQLException e) {
+			    logger.severe(e.getMessage());
+			    throw new PluginException("");
+		    }
+	    }
 
 
-}
+    }
 }
--- FCMExtender/fcm/model/Regole.cs
+++ FCMExtender/fcm/model/Regole.cs
@@ -38,85 +38,91 @@ namespace fcm.model
         public Dictionary<string, Modulo> moduli = new Dictionary<string, Modulo>();
         public Regole(OdbcConnection conn, string compSel)
         {
-            OdbcCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT puntipervittoria, fattorecampo, regolaportiere, regoladifesa, regolacentmedia, " +
+            using (OdbcCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT puntipervittoria, fattorecampo, regolaportiere, regoladifesa, regolacentmedia, " +
                     "regolacentdiffe, regoladiff4, regoladiff10, regolamin60, usaspeciale1, " +
                     "usaspeciale2, usaspeciale3, regolaattacco, regoladelta3, regolamin59," +
                     "regolaDiff4Valore, regolaDiff10Valore, regolaMin60Valore, regolaMin60Delta, regolaDelta3Valore, " +
                     "regolaMin59Valore, regolaMin59Delta, regolaMin59Almeno, VUCentrocampista, regolaDifesaVU," +
                     "VUDifensore FROM competizione WHERE id = " + compSel;
-            OdbcDataReader rea = cmd.ExecuteReader();
-            while (rea.Read())
-            {
-                puntiPerVittoria = rea.GetInt32(1);
-                fattoreCampo = rea.GetDouble(2);
-                regolaPortiere = rea.GetBoolean(3);
-                regolaDifesa = rea.GetBoolean(4);
-                regolaCentMedia = rea.GetBoolean(5);
-                regolaCentDiffe = rea.GetBoolean(6);
-                regolaDiff4 = rea.GetBoolean(7);
-                regolaDiff10 = rea.GetBoolean(8);
-                regolaMin60 = rea.GetBoolean(9);
-                usaSpeciale1 = rea.GetBoolean(10);
-                usaSpeciale2 = rea.GetBoolean(11);
-                usaSpeciale3 = rea.GetBoolean(12);
-                regolaAttacco = rea.GetBoolean(13);
-                regolaDelta3 = rea.GetBoolean(14);
-                regolaMin59 = rea.GetBoolean(15);
-                regolaDiff4Valore = rea.GetDouble(16);
-                regolaDiff10Valore = rea.GetDouble(17);
-                regolaMin60Valore = rea.GetDouble(18);
-                regolaMin60Delta = rea.GetDouble(19);
-                regolaDelta3Valore = rea.GetDouble(20);
-                regolaMin59Valore = rea.GetDouble(21);
-                regolaMin59Delta = rea.GetDouble(22);
-                regolaMin59Almeno = rea.GetDouble(23);
-                VUCentrocampista = rea.GetDouble(24);
-                regolaDifesaVU = rea.GetBoolean(25);
-                VUDifensore = rea.GetDouble(26);
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    while (rea.Read())
+                    {
+                        puntiPerVittoria = rea.GetInt32(1);
+                        fattoreCampo = rea.GetDouble(2);
+                        regolaPortiere = rea.GetBoolean(3);
+                        regolaDifesa = rea.GetBoolean(4);
+                        regolaCentMedia = rea.GetBoolean(5);
+                        regolaCentDiffe = rea.GetBoolean(6);
+                        regolaDiff4 = rea.GetBoolean(7);
+                        regolaDiff10 = rea.GetBoolean(8);
+                        regolaMin60 = rea.GetBoolean(9);
+                        usaSpeciale1 = rea.GetBoolean(10);
+                        usaSpeciale2 = rea.GetBoolean(11);
+                        usaSpeciale3 = rea.GetBoolean(12);
+                        regolaAttacco = rea.GetBoolean(13);
+                        regolaDelta3 = rea.GetBoolean(14);
+                        regolaMin59 = rea.GetBoolean(15);
+                        regolaDiff4Valore = rea.GetDouble(16);
+                        regolaDiff10Valore = rea.GetDouble(17);
+                        regolaMin60Valore = rea.GetDouble(18);
+                        regolaMin60Delta = rea.GetDouble(19);
+                        regolaDelta3Valore = rea.GetDouble(20);
+                        regolaMin59Valore = rea.GetDouble(21);
+                        regolaMin59Delta = rea.GetDouble(22);
+                        regolaMin59Almeno = rea.GetDouble(23);
+                        VUCentrocampista = rea.GetDouble(24);
+                        regolaDifesaVU = rea.GetBoolean(25);
+                        VUDifensore = rea.GetDouble(26);
 
-                FCMExtender.log("puntiPerVittoria: " + puntiPerVittoria);
-                FCMExtender.log("fattoreCampo: " + fattoreCampo);
-                FCMExtender.log("regolaPortiere: " + regolaPortiere);
-                FCMExtender.log("regolaDifesa: " + regolaDifesa);
-                FCMExtender.log("regolaCentMedia: " + regolaCentMedia);
-                FCMExtender.log("regolaCentDiffe: " + regolaCentDiffe);
-                FCMExtender.log("regolaDiff4: " + regolaDiff4);
-                FCMExtender.log("regolaDiff10: " + regolaDiff10);
-                FCMExtender.log("regolaMin60: " + regolaMin60);
-                FCMExtender.log("usaSpeciale1: " + usaSpeciale1);
-                FCMExtender.log("usaSpeciale2: " + usaSpeciale2);
-                FCMExtender.log("usaSpeciale3: " + usaSpeciale3);
-                FCMExtender.log("regolaAttacco: " + regolaAttacco);
-                FCMExtender.log("regolaDelta3: " + regolaDelta3);
-                FCMExtender.log("regolaMin59: " + regolaMin59);
-                FCMExtender.log("regolaDiff4Valore: " + regolaDiff4Valore);
-                FCMExtender.log("regolaDiff10Valore: " + regolaDiff10Valore);
-                FCMExtender.log("regolaMin60Valore: " + regolaMin60Valore);
-                FCMExtender.log("regolaMin60Delta: " + regolaMin60Delta);
-                FCMExtender.log("regolaDelta3Valore: " + regolaDelta3Valore);
-                FCMExtender.log("regolaMin59Valore: " + regolaMin59Valore);
-                FCMExtender.log("regolaMin59Delta: " + regolaMin59Delta);
-                FCMExtender.log("regolaMin59Almeno: " + regolaMin59Almeno);
-                FCMExtender.log("VUCentrocampista: " + VUCentrocampista);
-                FCMExtender.log("regolaDifesaVU: " + regolaDifesaVU);
-                FCMExtender.log("VUDifensore: " + VUDifensore);
+                        FCMExtender.log("puntiPerVittoria: " + puntiPerVittoria);
+                        FCMExtender.log("fattoreCampo: " + fattoreCampo);
+                        FCMExtender.log("regolaPortiere: " + regolaPortiere);
+                        FCMExtender.log("regolaDifesa: " + regolaDifesa);
+                        FCMExtender.log("regolaCentMedia: " + regolaCentMedia);
+                        FCMExtender.log("regolaCentDiffe: " + regolaCentDiffe);
+                        FCMExtender.log("regolaDiff4: " + regolaDiff4);
+                        FCMExtender.log("regolaDiff10: " + regolaDiff10);
+                        FCMExtender.log("regolaMin60: " + regolaMin60);
+                        FCMExtender.log("usaSpeciale1: " + usaSpeciale1);
+                        FCMExtender.log("usaSpeciale2: " + usaSpeciale2);
+                        FCMExtender.log("usaSpeciale3: " + usaSpeciale3);
+                        FCMExtender.log("regolaAttacco: " + regolaAttacco);
+                        FCMExtender.log("regolaDelta3: " + regolaDelta3);
+                        FCMExtender.log("regolaMin59: " + regolaMin59);
+                        FCMExtender.log("regolaDiff4Valore: " + regolaDiff4Valore);
+                        FCMExtender.log("regolaDiff10Valore: " + regolaDiff10Valore);
+                        FCMExtender.log("regolaMin60Valore: " + regolaMin60Valore);
+                        FCMExtender.log("regolaMin60Delta: " + regolaMin60Delta);
+                        FCMExtender.log("regolaDelta3Valore: " + regolaDelta3Valore);
+                        FCMExtender.log("regolaMin59Valore: " + regolaMin59Valore);
+                        FCMExtender.log("regolaMin59Delta: " + regolaMin59Delta);
+                        FCMExtender.log("regolaMin59Almeno: " + regolaMin59Almeno);
+                        FCMExtender.log("VUCentrocampista: " + VUCentrocampista);
+                        FCMExtender.log("regolaDifesaVU: " + regolaDifesaVU);
+                        FCMExtender.log("VUDifensore: " + VUDifensore);
+                    }
+                }
             }
 
-            cmd = conn.CreateCommand();
-            cmd.CommandText = "select numDife, numCent, numAtta, Modif, ModifAvv " +
+            using (OdbcCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "select numDife, numCent, numAtta, Modif, ModifAvv " +
                 "from Modulo, ModuloAmmesso " +
                 "WHERE ModuloAmmesso.IDCompetizione = " + compSel +
                 " and ModuloAmmesso.IDModulo=Modulo.id";
-
-
-            rea = cmd.ExecuteReader();
-            while (rea.Read())
-            {
-                string mod = rea.GetInt32(1) + "-" + rea.GetString(2) + "-" + rea.GetString(3);
-                moduli.Add(mod, new Modulo(rea.GetDouble(4), rea.GetDouble(5)));
+                using (OdbcDataReader rea = cmd.ExecuteReader())
+                {
+                    while (rea.Read())
+                    {
+                        string mod = rea.GetInt32(1) + "-" + rea.GetString(2) + "-" + rea.GetString(3);
+                        moduli.Add(mod, new Modulo(rea.GetDouble(4), rea.GetDouble(5)));
+                    }
+                    FCMExtender.log("Moduli: " + moduli);
+                }
             }
-            FCMExtender.log("Moduli: " + moduli);
         }
     }
 }
